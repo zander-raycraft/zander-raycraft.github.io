49a1db915bc74c8982fd129119b7fc67fc59482b
zander-raycraft <zanderraycraft504@gmail.com>
2023-06-12 13:51:06 -0500

    added in systems

--- website2/src/helperFiles/skillPage.js
+++ website2/src/helperFiles/skillPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./skillPage.css";
 import { FaTools, FaAngleDoubleRight } from 'react-icons/fa';
+import { DiTerminal } from 'react-icons/di';
 
 
 
@@ -13,17 +14,24 @@ export const SkillPage = () => {
                 alt="firstImage"
                 className="backImage"
                 />
+                <h1 className="skills">Skills</h1>
+                <h1 className="shadowSkill">Skills</h1>
                 <div className="section-one">
                 </div>
                 <div className="section-two">
                     <h1><FaTools/> Tools</h1>
-                    <h2><span><FaAngleDoubleRight /></span> React, Vite</h2>
+                    <h2><span><FaAngleDoubleRight /></span> React, Vite, express.JS, Prisma</h2>
                     <h2><span><FaAngleDoubleRight /></span> Git</h2>
                     <h2><span><FaAngleDoubleRight /></span> AWS</h2>
                     <h2><span><FaAngleDoubleRight /></span> Figma</h2>
                     <h2><span><FaAngleDoubleRight /></span> FireBase, MongoDB, mySQL</h2>
                 </div>
-                <div className="section-three"></div>
+                <div className="section-three">
+                    <h1><DiTerminal /> Systems</h1>
+                    <h2><span><FaAngleDoubleRight /></span> MacOS/Linux</h2>
+                    <h2><span><FaAngleDoubleRight /></span> Windows</h2>
+                    
+                </div>
             </div>
         </>
     );
