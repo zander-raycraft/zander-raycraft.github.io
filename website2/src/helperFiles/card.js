5634d4036e55418343ebc3f1691812084be9a3e2
zander-raycraft <zanderraycraft504@gmail.com>
2023-06-13 17:29:35 -0500

    finished updating the card click

--- website2/src/helperFiles/card.js
+++ website2/src/helperFiles/card.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+import { AiFillProject } from "react-icons/ai";
 import { Modal, Box, Typography, Button } from '@mui/material';
 import './card.css'; // Import the CSS file
 
-export const Card = ({ title, description, relativeImagePath }) => {
+export const Card = ({ title, description, Language, relativeImagePath }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleOpen = () => {
@@ -18,21 +19,41 @@ export const Card = ({ title, description, relativeImagePath }) => {
         backgroundImage: `url(${process.env.PUBLIC_URL}${relativeImagePath})`,
     };
 
+    const closeButtonStyle = {
+        position: 'absolute',
+        top: '10px',
+        right: '5px',
+        fontSize: '20px',
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        color: 'white',
+        border: 'none',
+        padding: '5px',
+        cursor: 'pointer',
+      };
+
     return (
       <div className="card" onClick={handleOpen} style={backgroundImageStyle}>
+        <AiFillProject className='icon' />
         <div className="cover">
           <h1>{ title }</h1>
         </div>
         {/* Card content */}
         <Modal open={isOpen} onClose={handleClose}>
           <Box className="modalContainer">
-            <Typography className='title' variant="h6" component="div" mb={2}>
+            <Typography className='title' variant="h5" component="div" mb={2}>
               {title}
             </Typography>
-            <Typography variant="body2">{description}</Typography>
-            <Button variant="contained" onClick={handleClose} sx={{ mt: 2 }}>
-              Close
-            </Button>
+            <Typography className="body-p" variant="body2">{description}</Typography>
+            <Typography className="body-l" variant="body2">{Language}</Typography>
+          <Button
+            className="close-button"
+            variant="contained"
+            onClick={handleClose}
+            style={closeButtonStyle}
+          >
+            X
+          </Button>
+
           </Box>
         </Modal>
       </div>
--- website2/src/helperFiles/projects.js
+++ website2/src/helperFiles/projects.js
@@ -5,24 +5,52 @@ import { Card } from "./card"
 export const ProjectsPage = () => {
     return (
         <>
-            <div className='main-container'>
+            <div className='main-container' style={{ 
+                backgroundImage: `url(${process.env.PUBLIC_URL}/webPics/shutterstock_514474840.webp)`, 
+                backgroundSize: 'cover', 
+                backgroundPosition: 'center', 
+                backgroundRepeat: 'no-repeat'
+                }}>
                 <div className='card-container'>
                     {/* Project One */}
                     <Card
                         title="Real Estate Analysis"
-                        description="Welcome to the stuff"
+                        description="The idea behind this project was to create a webscraper to
+                        get information and throw that information into a machine learning model,
+                        which will then run a mulitple linear regression to predict the price of
+                        the house within the following 12 months. This was based on a multitude of
+                        factors such as neighboring price, details of the house, estimated rehab budget
+                        and 3 year history of sales within that area."
+                        Language="Tools/Languages used: Python (ML Model),
+                        HTML/CSS, C++ (webscraping)."
                         relativeImagePath="/webPics/realEstate.jpeg"
                     />
                     {/* Project Two */}
                     <Card
                         title="Beckers Research Lab"
-                        description="Welcome to the stuff"
+                        description="This project works in partner with Vanderbilt's Department
+                        of Computer Science. I currently work as a Student Resarcher in Thomas Beckers's
+                        lab. The goal of this lab is to study the integration between machine learning 
+                        and control theory in soft robotics. My role in this lab is developing the soft robot
+                        as well as working to create a physics based machine learing model that acts as a feedback
+                        loop for the robot to evaluate and adjust its current action relative to where it 
+                        should be. Another task is improving the labs testing software to allow for a more accurate 
+                        rendering and anlysis of data."
+                        Language="Tools/Languages used: Python (ML Model). C++/MatLab (Analysis software)"
                         relativeImagePath="/webPics/mlbackground.jpeg"
                     />
                     {/* Project Three */}
                     <Card
                         title="MyStar Health tracker"
-                        description="Welcome to the stuff"
+                        description="The WebApp project is a tracker for someones mental and physical health. 
+                        The idea behind this is the user creates an account and fills out a form of their current fitness
+                        levels and their goal fitness levels. They are then given a basic workout plan to start from. 
+                        As the user logs their workouts and progress, the workout plan re-evaluates what is best for them 
+                        to best meet their goals. There is a second section that focuses on mental health where the user
+                        fils out a daily for, logging how they felt throughout the day. They can look at their history and if they
+                        opt into it, they can have simple advice generated for them with the aim of helping with whatever their mental 
+                        state is."
+                        Language="Tools/Languages used: React (JS Framework), Netlify (web hosting), HTML/CSS, JavaScript (App development)"
                         relativeImagePath="/webPics/healthImage.jpeg"
                     />
                     {/* Project Three */}
@@ -31,6 +59,11 @@ export const ProjectsPage = () => {
                         description="Welcome to the stuff"
                         relativeImagePath="/webPics/AE91CF11-A476-4D7B-BD7C-52EB0C1064DA.JPG"
                     />
+                    <div className='intro-text'>
+                        <h1 className="top-text">Projects</h1>
+                        <h1 className="bottom-text">Projects</h1>
+                    </div>
+                    <div className='time-bar' />
                 </div>
             </div>
         </>
